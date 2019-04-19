c1ce67b9b5757769869b117d8b470d177cccee63
谢宇轩 <xieyuxuan@netpas.co>
2019-04-19 18:52:35 +0800

    添加PSR-4自动加载规则

---
+++ app/Common/BaseSpider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Common;
+
+/**
+ * Created by PhpStorm.
+ * User: YiChu
+ * Date: 2019/4/19
+ * Time: 18:48
+ */
+
+class BaseSpider{
+
+}
---
+++ app/Spider/DemoSpider.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Common\BaseSpider;
+
+/**
+ * Created by PhpStorm.
+ * User: YiChu
+ * Date: 2019/4/19
+ * Time: 18:47
+ */
+
+class DemoSpider extends BaseSpider {
+
+}
